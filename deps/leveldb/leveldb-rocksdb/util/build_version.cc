194f9f756813bd7ecc2eeb122e3e7e233f373dc3
Thibault Riviere <thibault.riviere@scality.com>
2016-04-06 15:04:10 +0200

    not so useless file

---
+++ deps/leveldb/leveldb-rocksdb/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:303e2c2a9f254a48b45398e66a1b19ef887f9874";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2016-04-06";
+const char* rocksdb_build_compile_date = __DATE__;
